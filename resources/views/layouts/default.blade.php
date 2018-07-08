6e9e2b7241a09b0526b3be3747ebf0b0e4e935be
zhangming1 <moresuccess2@gmail.com>
2018-07-08 14:04:38 +0000

    Finish exam static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html">
+    <title>@yield('title','Sample App') - Laravel 新手入门教程</title>
+</head>
+<body>
+ @yield('content');
+</body>
+</html>
